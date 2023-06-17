ef3d8e5ce41190f034da889f5f273543c8c81169
Hayato <hayato.314.nico@gmail.com>
2023-06-18 08:15:45 +0900

    TOPページの作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,83 +1,52 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
-</template>
+  <v-sheet
+    elevation="12"
+    max-width="600"
+    rounded="lg"
+    width="100%"
+    class="pa-4 text-center mx-auto mt-4"
+  >
+    <h2 class="text-h5 mb-6">ポケモン自己診断</h2>
 
-<script>
+    <p class="mb-4 text-medium-emphasis text-body-2">
+      ポケモン自己診断っぽいものを作ってみました！
+      <br />
+      遊んでね！
+    </p>
+    <!-- 罫線(不要なので外す)
+      <v-divider class="mb-4"></v-divider> -->
+
+    <div class="text-center">
+      <v-btn
+        class="text-none"
+        color="yellow"
+        rounded
+        variant="flat"
+        width="200"
+        to="/questions"
+      >
+        　診断をはじめる
+      </v-btn>
+    </div>
+  </v-sheet>
+</template>
+<style>
+.custom-link {
+  text-decoration: none;
+  color: black;
+}
+</style>
+<script lang="js">
+
 export default {
-  name: 'IndexPage'
+  data () {
+    return {
+    }
+  },
+  methods: {
+    startAnalysis () {
+
+    }
+  }
 }
 </script>
